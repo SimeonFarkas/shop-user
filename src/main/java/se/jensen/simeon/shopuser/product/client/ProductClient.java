e6ba3016cb9dc2af46059ca08e0195b4ab55e35e
SimeonFarkas <simeon.s.farkas@gmail.com>
2026-06-11 15:28:07 +0200

    Add JWT service token for ProductService communication

--- src/main/java/se/jensen/simeon/shopuser/product/client/ProductClient.java
+++ src/main/java/se/jensen/simeon/shopuser/product/client/ProductClient.java
@@ -3,6 +3,7 @@ package se.jensen.simeon.shopuser.product.client;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestClient;
 import se.jensen.simeon.shopuser.product.model.Product;
+import se.jensen.simeon.shopuser.security.JwtUtil;
 
 import java.util.Arrays;
 import java.util.List;
@@ -10,20 +11,28 @@ import java.util.List;
 @Component
 public class ProductClient {
     private final RestClient restClient;
-    private final String productServiceUrl = "http://localhost:8081/api/products";
+    private final JwtUtil jwtUtil;
 
-    public ProductClient(RestClient.Builder builder) {
+    public ProductClient(RestClient.Builder builder, JwtUtil jwtUtil) {
         this.restClient = builder
-                .baseUrl(productServiceUrl)
+                .baseUrl("http://localhost:8081")
                 .build();
+        this.jwtUtil = jwtUtil;
     }
 
     public List<Product> getProducts() {
-        Product[] products = restClient
+        String token = jwtUtil.generateServiceToken();
+
+        var request = restClient
                 .get()
-                .uri(productServiceUrl)
+                .uri("/api/products");
+
+        var requestWithHeader = request.header("Authorization", "Bearer " + token);
+
+        Product[] products = requestWithHeader
                 .retrieve()
                 .body(Product[].class);
+
         if (products == null || products.length == 0) {
             return List.of();
         }
